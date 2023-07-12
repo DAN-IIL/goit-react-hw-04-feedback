587025dafe755f5dd1f756fa0e6ca36dad9984de
Danya <99263066+DAN-IIL@users.noreply.github.com>
2023-07-13 00:08:32 +0300

    Push 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
+import { useState } from 'react';
+import { Statistics } from './Statistics/Statistics';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import { Container } from './App.styled';
+
 export const App = () => {
+
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const feedbackOptions = ["good", "neutral", "bad"];
+
+  const handleChangeState = e => {
+    const { name } = e.currentTarget;
+    switch(name) {
+      case "good":
+        setGood(prevSet => prevSet + 1)
+      break;
+      case "neutral":
+        setNeutral(prevSet => prevSet + 1)
+      break;
+      case "bad":
+        setBad(prevSet => prevSet + 1)
+      break;
+
+      default:
+        console.log(`This is opion ${name} in not support`)
+    }
+  };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good * 100) / (good + neutral + bad));
+  };
+
+  const totalOpinions = countTotalFeedback();
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={feedbackOptions}
+          onLeaveFeedback={handleChangeState}
+        />
+      </Section>
+      {totalOpinions <= 0 ? (
+        <Notification message="There is no feedback" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      )}
+    </Container>
   );
-};
+}
---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const SectionsStatistic = styled.section`
+font-weight: 600;
+    text-align: center;
+    margin-bottom: 24px;
+    text-shadow: 0 0 8px #e82ff1;
+`
+
+export const SectionsTitle = styled.h2`
+font-weight: 600;
+    text-align: center;
+    font-weight: 700;
+    font-size: 24px;
+    text-shadow: 0 0 8px #e82ff1;
+`
